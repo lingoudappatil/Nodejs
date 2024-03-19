324eb7b7a1a2c6c8246113d884979c1a5ab69f02
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-03-19 08:52:12 +0000

    server

---
+++ HTTP_Server/fs_server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const fs = require("fs");
+const myServer =  http.createServer((req,res)=>
+{
+    const log = '${Date.now()}:New request recieved\n';
+    fs.appendFile("log.txt",log,(err,data)=>
+    {
+        res.end("Hello from ");
+    });
+});
+myServer.listen(8000,()=> console.log("Server Started"));
---
+++ HTTP_Server/server.js
@@ -0,0 +1,8 @@
+//creating http server by using http module
+const http = require("http");
+const myServer=http.createServer((req,res) =>
+{
+    console.log("New request");
+    res.end("Hello from Server");
+});
+myServer.listen(8000,() => console.log("Server Started"));
