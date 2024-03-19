75a2bfa4a08b40b4abe4462c5014add773bbc034
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-03-19 10:09:51 +0000

    http

--- HTTP_Server/fs_server.js
+++ HTTP_Server/fs_server.js
@@ -5,7 +5,7 @@ const myServer =  http.createServer((req,res)=>
     const log = '${Date.now()}:New request recieved\n';
     fs.appendFile("log.txt",log,(err,data)=>
     {
-        res.end("Hello from ");
+        res.end("Hello from server");
     });
 });
 myServer.listen(8000,()=> console.log("Server Started"));
---
+++ HTTP_Server/fs_url.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const fs = require("fs");
+const myServer =  http.createServer((req,res)=>
+{
+    const log = '${Date.now()}:New request recieved\n';
+    fs.appendFile("log.txt",log,(err,data)=>
+    {
+        switch(req.url)
+        {
+            case '/': res.end("Home Page");
+            break
+            case '/about': res.end("This about the Page");
+            break
+            default: res.end("Not Found");
+        }
+    });
+});
