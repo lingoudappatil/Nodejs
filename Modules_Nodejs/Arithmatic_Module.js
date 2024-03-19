878f24e4053cd5be40e1eeb0d639229729167e5b
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-03-19 02:08:48 +0000

    mod

---
+++ Modules_Nodejs/Arithmatic_Module.js
@@ -0,0 +1,17 @@
+function add(a,b)
+{
+    return a+b;
+}
+function sub(a,b)
+{
+    return a-b;
+}
+function mult(a,b)
+{
+    return a*b;
+}
+function mod(a,b)
+{
+    return a%b;
+}
+module.exports={add,sub,mult,mod};
