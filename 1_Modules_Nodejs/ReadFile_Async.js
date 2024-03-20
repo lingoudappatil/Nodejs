9af2451efd2524e59b6cb7783d00bb7b40a740ba
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-03-20 18:13:01 +0000

    async

---
+++ 1_Modules_Nodejs/ReadFile_Async.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const res = fs.readFileSync("./fswrite.txt","utf-8",(err,result)=>
+{
+    if(err)
+    {
+        console.log("Errorr",err);
+    }else
+    {
+        console.log("result");
+    }
+});
