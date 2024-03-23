d7a73d9dbe21c9e2bb326602c4cbbdd33eff97e8
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-03-23 16:14:02 +0000

    n

---
+++ 3_NPM/npm_simp.js
@@ -0,0 +1,9 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  /*Use our upper-case module to upper case a string:*/
+  res.write(uc.upperCase("Hello World!"));
+  res.end();
+}).listen(8080);
+
+              
