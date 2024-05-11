d8bcae554a40b3812428dc70d5679a66e3f46e1b
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-05-11 17:42:14 +0000

    changes

---
+++ 5_RestAPI/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+
+const app = express();
+const port = 8000;
+
+app.get('/users', (req,res) =>
+{
+    return res.json(users);
+});
+app.listen(port,() => console.log("Server Started"));
