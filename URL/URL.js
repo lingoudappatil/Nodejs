ffa57a293386db589f8de0e2f7debd41dae08a53
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-07-06 23:06:27 +0530

    URL.js

--- URL/URL.js
+++ URL/URL.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const url = require("url");   
 
 const myServer =  http.createServer((req,res)=>
-{
+{ 
     if(req.url === '/favicon.ico') return res.end();
     const log = `${Date.now()}: ${req.url} New request received\n`;
     const myUrl = url.parse(req.url,true);
