f7503a848e32aba79e038c4bb52085cab7c643b1
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-07-13 23:22:56 +0530

    file.js

--- 3_NPM/file.js
+++ 3_NPM/file.js
@@ -3,7 +3,7 @@ var formidable = require('formidable');
    
 http.createServer(function (req, res) {
   if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
+    var form = new formidable.IncomingForm(); 
     form.parse(req, function (err, fields, files) {
       res.write('File uploaded');
       res.end();
