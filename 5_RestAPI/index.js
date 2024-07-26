7e51c32636134d11cc35d0bcbfa470c67443d2f3
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-07-26 14:24:14 +0530

    index.js

--- 5_RestAPI/index.js
+++ 5_RestAPI/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const users = require("./MOCK_DATA.json");
  
 const app = express();
-const port = 8000;
+const port = 8000; 
    
 app.get('/users', (req,res) =>
 {
